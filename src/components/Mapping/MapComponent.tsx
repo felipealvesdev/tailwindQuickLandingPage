72a9d4f619e4b9fc2815a16ba067ab21ededdbad
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2024-11-28 19:39:10 -0300

    feat: add MapComponent translation

--- src/components/Mapping/MapComponent.tsx
+++ src/components/Mapping/MapComponent.tsx
@@ -6,9 +6,16 @@ import "leaflet/dist/leaflet.css";
 import { StationsResponse } from "@/interfaces/ApiResponse";
 import { MapComponentProps } from "@/interfaces/MapComponentsInterfaces";
 import { Link } from "react-router-dom";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useState } from "react";
 import { ListRestart } from "lucide-react";
+import useTranslation from "@/hooks/useTranslation";
 
 const DefaultIcon = L.icon({
   iconUrl: icon,
@@ -37,23 +44,59 @@ export default function MapComponent({
   scrollWheelZoom = false,
   markers,
 }: MapComponentProps) {
+  const { language } = useTranslation();
   const [select, setselect] = useState<string | undefined>("");
-  const filteredMarkers = select? markers?.filter((marker) => String(marker.id) === select): markers;
+  const filteredMarkers = select
+    ? markers?.filter((marker) => String(marker.id) === select)
+    : markers;
 
   const handleReset = () => {
     setselect("");
   };
 
+  const translateObject = {
+    title: {
+      "pt-BR": "Estações",
+      "en-US": "Stations",
+    },
+    cartTitle: {
+      "pt-BR": "Buscar Estações",
+      "en-US": "Search Stations",
+    },
+    resetSelection: {
+      "pt-BR": "Resetar Seleção",
+      "en-US": "Clear Selection",
+    },
+    selectPlaceholder: {
+      "pt-BR": "Selecione uma estação",
+      "en-US": "Select a station",
+    },
+  };
+
   return (
     <div className="w-full max-w-3xl mx-auto p-4 bg-white dark:bg-gray-900 rounded-lg shadow-md">
-      <h2 className="text-center text-2xl sm:text-3xl font-semibold mb-6">Estações</h2>
+      <h2 className="text-center text-2xl sm:text-3xl font-semibold mb-6">
+        {language === "pt-BR"
+          ? translateObject.title["pt-BR"]
+          : translateObject.title["en-US"]}
+      </h2>
       <div className="w-full bg-gray-50 p-4 rounded-lg shadow-md mb-4">
-        <h3 className="text-lg font-semibold mb-4">Buscar Estação</h3>
+        <h3 className="text-lg font-semibold mb-4">
+          {language === "pt-BR"
+            ? translateObject.cartTitle["pt-BR"]
+            : translateObject.cartTitle["en-US"]}
+        </h3>
         <div className="flex items-center space-x-2">
           <div className="flex w-full items-center space-x-2">
             <Select value={select} onValueChange={setselect}>
-              <SelectTrigger className="w-full flex flex-1"> 
-                <SelectValue placeholder="Selecione uma estação" />
+              <SelectTrigger className="w-full flex flex-1">
+                <SelectValue
+                  placeholder={
+                    language === "pt-BR"
+                      ? translateObject.selectPlaceholder["pt-BR"]
+                      : translateObject.selectPlaceholder["en-US"]
+                  }
+                />
               </SelectTrigger>
               <SelectContent className="max-h-60 overflow-y-auto">
                 {markers && renderSelectItems(markers)}
@@ -62,7 +105,11 @@ export default function MapComponent({
             <button
               onClick={handleReset}
               className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full hover:bg-gray-300"
-              title="Resetar seleção"
+              title={
+                language === "pt-BR"
+                  ? translateObject.resetSelection["pt-BR"]
+                  : translateObject.resetSelection["en-US"]
+              }
             >
               <ListRestart className="w-5 h-5 text-gray-700" />
             </button>
